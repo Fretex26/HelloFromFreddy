61aaeb0e13bb44910be85db501270a0e1eefae20
JacobDeMille <jacob.demille@icloud.com>
2020-10-08 11:29:37 -0600

    Commit from Jacob

--- src/helloGit/Main.java
+++ src/helloGit/Main.java
@@ -3,6 +3,7 @@ package helloGit;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello, my name is Freddy");
+        System.out.println("Hello, my name is Freddy");
+        System.out.println("Hello Freddy, this is Jacob!");
     }
 }
