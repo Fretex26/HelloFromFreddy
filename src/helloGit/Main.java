c8340373aa08561039b49f3fc86d16702bd7f7c6
Valerie Ludden <valudden@gmail.com>
2020-10-08 12:28:52 -0600

    Hi Frddy from Val

--- src/helloGit/Main.java
+++ src/helloGit/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Freddy");
         System.out.println("Hello Freddy, this is Jacob!");
+        System.out.println("Hi Freddy, this is Valerie!");
     }
 }
